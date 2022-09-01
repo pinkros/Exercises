e72a954a5e39716b5e915a0c9fca636a98219fbc
pinkros <arvid.leimar@iths.se>
2022-09-01 11:01:34 +0200

    övning 1-4

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,2 +1,37 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int loopVar = 0;
+int input = 0;
+Console.WriteLine("Skriv ett heltal:");
+
+//While loop för att kunna testa koden flera gånger på samma gång, samma med loopVar i if-satserna. 
+//while (input != 666)
+//{
+    while (loopVar == 0)
+    {
+        try
+        {
+            input = Int32.Parse(Console.ReadLine());
+            loopVar++;
+        }
+        catch
+        {
+            Console.WriteLine("det var inte vad jag bad om, försök igen!");
+            loopVar = 0;
+        }
+    }
+
+    if (input < 100)
+    {
+        Console.WriteLine("Ditt tal är mindre än 100.");
+        //loopVar = 0;
+    }
+    else if (input > 100)
+    {
+        Console.WriteLine("Ditt tal är större än hundra.");
+        //loopVar = 0;
+    }
+    else
+    {
+        Console.WriteLine("Ditt tal är exakt hundra.");
+        //loopVar = 0;
+    }
+//}
---
+++ Exercise4/Program.cs
@@ -0,0 +1,26 @@
+﻿int loopVar = 0;
+var times = 0;
+
+Console.WriteLine("Hej! Vad heter du?");
+string name = Console.ReadLine();
+
+Console.WriteLine("Hur många hälsningar vill du ha?");
+
+while (loopVar == 0)
+{
+    try
+    {
+        times = Int32.Parse(Console.ReadLine());
+        loopVar++;
+    }
+    catch
+    {
+        Console.WriteLine("det var inte vad jag bad om, försök igen!");
+        loopVar = 0;
+    }
+}
+for (int i = 0; i < times; i++)
+{
+    Console.WriteLine($"Hej, {name}!");
+}
+
