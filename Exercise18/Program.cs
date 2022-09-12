32eb35138619a7957a6d2278565ca7b78a34be81
pinkros <arvid.leimar@iths.se>
2022-09-12 14:19:06 +0200

    18 klar

--- Exercise18/Program.cs
+++ Exercise18/Program.cs
@@ -1,5 +1,23 @@
 ﻿// Be användaren mata in en mening.
 // Skriv ut det genomsnittliga antalet bokstäver i orden.
 
+char[] separators = new char[] { ' ', '.' };
+var sum = 0.0;
+var amount = 0.0;
+
 Console.WriteLine("Skriv in en mening:");
+
 string sentence = Console.ReadLine();
+
+string[] words = sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var word in words)
+{
+    sum += word.Length;
+    amount++;
+}
+
+
+double mean = sum / amount;
+
+Console.WriteLine($"Den genomsnittsliga längden på orden i meningen är: {Math.Round(mean, 2)}");
