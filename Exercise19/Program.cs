7eb61681247215140af60b60da0b7c8e7b30bf93
pinkros <arvid.leimar@iths.se>
2022-09-12 14:36:14 +0200

    19 klar

---
+++ Exercise19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Skriv en metod DrawBox(int width, int height)
+// När man anropar metoden ska den tömma konsolen och skriva ut en rektangel där de yttre tecknen består av ‘#’ och de inre av ‘-’
+
+DrawBox(7, 7);
+
+void DrawBox(int width, int height) 
+{
+    Console.Clear();
+
+	for (int i = 0; i < height; i++)
+	{
+		if (i == 0 || i + 1 == height) 
+		{
+			for (int j = 0; j < width; j++)
+			{
+				Console.Write("#");
+			}
+			Console.Write("\n");
+		}
+
+		else
+		{
+			Console.Write("#");
+			for (int j = 0; j < width - 2; j++)
+			{
+				Console.Write("-");
+			}
+            Console.Write("#\n");
+        }
+	}
+}
