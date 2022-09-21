16049b7e2df5e8ad06b0ad2284509b16ee0a296e
pinkros <arvid.leimar@iths.se>
2022-09-21 09:40:05 +0200

    24

---
+++ Exercise24/Program.cs
@@ -0,0 +1,78 @@
+﻿//a.
+//#-------
+//-#------
+//--#-----
+//---#----
+//for (int i = 0; i < 4; i++)
+//{
+//	for (int j = 0; j < 8; j++)
+//	{
+//        if (i == j)
+//        {
+//            Console.Write("#");
+//        }
+//        else
+//        {
+//            Console.Write("-");
+//        }
+
+
+//    }
+//    Console.Write("\n");
+//}
+
+//b.
+//#---#---
+//-#---#--
+//--#---#-
+//---#---#
+
+//for (int i = 0; i < 4; i++)
+//{
+//    for (int j = 0; j < 8; j++)
+//    {
+//        if (i == j)
+//        {
+//            Console.Write("#");
+//        }
+
+//        else if (j == i+4)
+//        {
+//            Console.Write("#");
+//        }
+//        else
+//        {
+//            Console.Write("-");
+//        }
+
+
+//    }
+//    Console.Write("\n");
+//}
+
+//c.
+//##------
+//--##----
+//----##--
+//------##
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        if (i == j)
+        {
+            Console.Write("#");
+        }
+        else if (j == i + 1)
+        {
+            Console.Write("#");
+        }
+        else
+                {
+            Console.Write("-");
+        }
+
+
+    }
+    Console.Write("\n");
+}
