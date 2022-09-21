ff07545529ef9a607cc503049e044ff431646c39
pinkros <arvid.leimar@iths.se>
2022-09-21 13:55:53 +0200

    24.e

--- Exercise24/Program.cs
+++ Exercise24/Program.cs
@@ -55,24 +55,75 @@
 //--##----
 //----##--
 //------##
+//int count = 0;
+//for (int i = 0; i < 4; i++)
+//{
+//    for (int j = 0; j < 7; j++)
+//    {
+//        if (j == count)
+//        {
+//            for (int k = 0; k < 2; k++)
+//            {
+//                Console.Write("#"); 
+//            }
+
+//        }
+//        else
+//        {
+//            Console.Write("-");
+//        }
+
+//    }
+//    count += 2;
+//    Console.Write("\n");
+//}
+
+//d.
+//--#--#--
+//--#--#--
+//########
+//--#--#--
+
+//for (int i = 0; i < 4; i++)
+//{
+//    for (int j = 0; j < 8; j++)
+//    {
+//        if (i == 2 || j == 2 || j == 5)
+//        {
+//            Console.Write("#");
+//        }
+
+//        else
+//        {
+//            Console.Write("-");
+//        }
+
+//    }
+
+//    Console.Write("\n");
+//}
+
+//e.
+//#---#---
+//-#-#----
+//--#-----
+//-#-#----
+
 for (int i = 0; i < 4; i++)
 {
     for (int j = 0; j < 8; j++)
     {
-        if (i == j)
+        if (i == 2 || j == 2 || j == 5)
         {
             Console.Write("#");
         }
-        else if (j == i + 1)
-        {
-            Console.Write("#");
-        }
+
         else
-                {
+        {
             Console.Write("-");
         }
 
-
     }
+
     Console.Write("\n");
 }
