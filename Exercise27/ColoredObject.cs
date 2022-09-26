a6489b5de2c748eb6d0b80464642da9f86007e48
pinkros <arvid.leimar@iths.se>
2022-09-26 11:07:24 +0200

    övning 27

---
+++ Exercise27/ColoredObject.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercise27;
+
+public class ColoredObject
+{
+    private float _red;
+    private float _blue;
+
+    public float Red
+    {
+        get { return _red; }
+        set
+        {
+            if (0 <= value && value <= 100)
+            {
+                _red = value;
+                _blue = 100 - value;
+            }
+
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+
+    public float Blue
+    {
+        get { return _blue; }
+        set
+        {
+            if (0 <= value && value <= 100)
+            {
+                _blue = value;
+                _red = 100 - value;
+            }
+
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ Exercise27/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Exercise27;
+
+ColoredObject car = new ColoredObject();
+
+car.Red = 21.5F;
+
+Console.WriteLine($"Röd: {car.Red}\nBlå: {car.Blue}");
+
+car.Blue = 65F;
+
+Console.WriteLine($"Röd: {car.Red}\nBlå: {car.Blue}");
+
